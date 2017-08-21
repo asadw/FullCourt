aa9a9148c8efc494cdccac498f036e8021765033
asadw <asad311@msn.com>
2017-08-21 00:48:56 -0400

    added a firebase import statement for github test

--- app/src/main/java/com/fullcourt/HomeScreenView.java
+++ app/src/main/java/com/fullcourt/HomeScreenView.java
@@ -15,6 +15,7 @@ import android.view.View;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
 
 /**
  * TODO: document your custom view class.
