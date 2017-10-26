7387722f43caefef67ea07d32166586e9e146669
root <asad311@msn.com>
2017-10-25 20:45:16 -0400

    test firestore code

--- app/src/main/java/com/fullcourt/MainActivity.java
+++ app/src/main/java/com/fullcourt/MainActivity.java
@@ -3,11 +3,51 @@ package com.fullcourt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.WriteResult;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Firestore db;
+
+    private MainActivity(){
+        String projectId = "projectId";
+        FirestoreOptions firestoreOptions =
+                FirestoreOptions.getDefaultInstance().toBuilder()
+                        .setProjectId(projectId)
+                        .build();
+        db = firestoreOptions.getService();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DocumentReference docRef = db.collection("users").document("alovelace");
+        // Add document data  with id "alovelace" using a hashmap
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+        // ...
+        // result.get() blocks on response
+        try {
+            System.out.println("Update time : " + result.get().getUpdateTime());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
