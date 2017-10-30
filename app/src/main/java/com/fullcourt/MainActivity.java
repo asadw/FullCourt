08f931587bb20b1d0aba9aceca151e2d63014333
asadw <asad311@msn.com>
2017-10-30 01:34:50 -0400

    import gms-places classes and add some sample code

--- app/src/main/java/com/fullcourt/MainActivity.java
+++ app/src/main/java/com/fullcourt/MainActivity.java
@@ -10,20 +10,35 @@ import java.util.concurrent.ExecutionException;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+
 public class MainActivity extends AppCompatActivity {
+
+    // private FusedLocationProviderClient client = new FusedLocationProviderClient(this);
+    protected GeoDataClient mGeoDataClient;
 
     public MainActivity(){
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello, Nevada!");
         myRef.push();
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        PlaceDetectionClient mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
     }
 }
