4d69d36873a610a3ef5647edbb6410dd1c40b484
asadw <asad311@msn.com>
2018-12-19 00:27:51 -0500

    add logic for authentication and storing UID to db

--- app/src/main/java/com/fullcourt/MainActivity.java
+++ app/src/main/java/com/fullcourt/MainActivity.java
@@ -2,8 +2,7 @@ package com.fullcourt;
 
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.Manifest;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.content.ContextCompat;
@@ -11,37 +10,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 
+import com.firebase.ui.auth.AuthUI;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
-
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.OnCompleteListener;
-
+import java.util.List;
+import java.util.Arrays;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     protected GeoDataClient mGeoDataClient;
-    private DatabaseReference myRef;
-    //private DatabaseReference myOtherRef;
+    private DatabaseReference uid_db_ref;
+    private DatabaseReference myOtherRef;
     private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private static final int RC_SIGN_IN = 123;
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
 
     private boolean mLocationPermissionGranted;
 
@@ -56,12 +62,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public MainActivity(){
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
-        DatabaseReference myOtherRef = database.getReference("some_data");
-        myOtherRef.setValue(System.currentTimeMillis());
-        myOtherRef.push();
     }
 
     @Override
@@ -80,24 +80,89 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         getLocationPermission();
 
+        final List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        final Button findCourtsBtn = findViewById(R.id.button);
+        findCourtsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openAutocompleteActivity();
+            }
+        });
+
         final Button button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                myRef.setValue(Calendar.getInstance().getTime().toString());
-                myRef.push();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                uid_db_ref = database.getReference(uid);
+                uid_db_ref.push();
+
+                myOtherRef = uid_db_ref.child("some_data");
+                myOtherRef.setValue(Calendar.getInstance().getTime().toString());
+                myOtherRef.push();
+            }
+        });
+
+        final Button signInBtn = findViewById(R.id.button3);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Create and launch sign-in intent
+                startActivityForResult(AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(), RC_SIGN_IN);
             }
         });
     }
 
+    private void openAutocompleteActivity() {
+        try {
+            // The autocomplete activity requires Google Play Services to be available. The intent
+            // builder checks this and throws an exception if it is not the case.
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this);
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // Indicates that Google Play Services is either not installed or not up to date. Prompt
+            // the user to correct the issue.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates that Google Play Services is not available and the problem is not easily
+            // resolvable.
+            String message = "Google Play Services is not available: " +
+                    GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e(TAG, message);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     /**
-     * Prompts the user for permission to use the device location.
+     * Called after the autocomplete activity has finished to return its result.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check that the result was from the autocomplete widget.
+        if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            Place place = PlaceAutocomplete.getPlace(this, data);
+            Log.i(TAG, "Place Selected: " + place.getName());
+        }
+    }
+
+    /**
+     * * Prompts the user for permission to use the device location.
      */
     private void getLocationPermission() {
         /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
+        * Request location permission, so that we can get the location of the
+        * device. The result of the permission request is handled by a callback,
+        * onRequestPermissionsResult.
+        */
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
         } else {
@@ -161,41 +226,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     /**
-     * Gets the current location of the device, and positions the map's camera.
-     */
-    private void getDeviceLocation() {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (mLocationPermissionGranted) {
-                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            mLastKnownLocation = task.getResult();
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                    new LatLng(mLastKnownLocation.getLatitude(),
-                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                        } else {
-                            Log.d(TAG, "Current location is null. Using defaults.");
-                            Log.e(TAG, "Exception: %s", task.getException());
-                            mMap.moveCamera(CameraUpdateFactory
-                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
-                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                        }
-                    }
-                });
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
-    }
-
-    /**
      * Updates the map's UI settings based on whether the user has granted location permission.
      */
     private void updateLocationUI() {
@@ -221,7 +251,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      * Manipulates the map when it's available.
      * This callback is triggered when the map is ready to be used.
      */
-    @Override
+/*    @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
 
@@ -259,5 +289,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Get the current location of the device and set the position of the map.
         getDeviceLocation();
-    }
+    }*/
 }
